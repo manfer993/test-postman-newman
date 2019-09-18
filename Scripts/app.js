1cb53e69f2d1a68cb17781d1b34d9d3fdf91cb36
Munoz, Manuel <Manuel.Munoz@experian.com>
2019-09-18 16:56:30 -0500

    agregar clase js

---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+var newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('../Collection/Scripts.postman_collection.json'),
+    environment: require('../Environment/Dev.postman_environment.json'),
+    globals: require('../Data/Workspace.postman_globals.json'),
+    reporters: ['cli','html','json'],
+    reporter : { 
+        html : { 
+            export : './Report/html/htmlReport.html', 
+            template: './Template/customTemplate.hbs'
+        }, 
+        json: { 
+            export: '../Report/json/jsonReport.json'
+        }
+    },
+    insecure: true, // allow self-signed certs, required in postman too
+    timeout: 180000  // set time out
+}).on('start', function (err, args) { // on start of run, log to console
+    console.log('running a collection...');
+}).on('done', function (err, summary) {
+    if (err || summary.error) {
+        console.error('collection run encountered an error.');
+    }
+    else {
+        console.log('collection run completed.');
+    }
+});
